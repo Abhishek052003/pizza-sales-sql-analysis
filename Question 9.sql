a649763762c505a5f25c28db80cda7bfd98aad6b
Analyst_Abhi <123734524+Abhishek052003@users.noreply.github.com>
2025-04-24 09:59:36 +0530

    Delete Question 9.sql

--- Question 9.sql
+++
@@ -1,8 +0,0 @@
--- 9.Group the orders by date and calculate the average number of pizzas ordered per day.
-
-select avg(quantity) from
-(select o.order_date, sum(od.quantity) as quantity
-from orders o
-join order_details od
-on o.order_id=od.order_id
-group by o.order_date) as ordered_quantity;
