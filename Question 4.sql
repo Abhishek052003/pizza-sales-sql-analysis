fa64de10d0a6a8c7674e2af4f5736db74f10c24b
Analyst_Abhi <123734524+Abhishek052003@users.noreply.github.com>
2025-04-24 09:58:43 +0530

    Delete Question 4.sql

--- Question 4.sql
+++
@@ -1,9 +0,0 @@
--- 4.Identify the most common pizza size ordered.
-
-select p.size, count(od.quantity) as Total_Time_Ordered
-from pizzas p 
-inner join order_details od 
-on p.pizza_id=od.pizza_id
-group by p.size
-order by count(od.quantity) desc
-limit 1;
