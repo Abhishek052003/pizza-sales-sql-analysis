8b4fc115ded2f340a8204aeb43e7834358a652e1
Analyst_Abhi <123734524+Abhishek052003@users.noreply.github.com>
2025-04-24 09:57:59 +0530

    Delete Question 12.sql

--- Question 12.sql
+++
@@ -1,9 +0,0 @@
--- 11.Analyze the cumulative revenue generated over time.
-
-select order_date ,revenue, sum(revenue) over (order by order_date) as cum_revenue from
-(select o.order_date, sum(od.quantity*p.price) as revenue
-from order_details od 
-join orders o on o.order_id=od.order_id
-join pizzas p on p.pizza_id=od.pizza_id
-group by o.order_date
-order by revenue) as sales;
